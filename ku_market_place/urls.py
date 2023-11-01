07fbb9387488bf67b4d07538ea5d4edce2680405
Phiranat Chaleekul <pnchalee@gmail.com>
2023-11-02 00:41:48 +0700

    update ui

--- ku_market_place/urls.py
+++ ku_market_place/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from ku_market_place.views import HomePageView
+from . import views
 
 
-app_name = "ku_market_place"
+app_name = "ku-market-place"
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('order_list/', views.order_list, name='order_list'),
+    path('product/', views.product, name='product'),
+    path('single_product/', views.single_product, name='single_product'),
 ]
--- ku_market_place/views.py
+++ ku_market_place/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from ku_market_place.models import Order
+from django.shortcuts import render
+
 
 # Create your views here.
 
@@ -10,3 +12,23 @@ class HomePageView(generic.ListView):
 
     def get_queryset(self):
         return Order.objects.all()
+
+
+def about(request):
+    return render(request, 'ku_market_place/about.html')
+
+
+def contact(request):
+    return render(request, 'ku_market_place/contact.html')
+
+
+def order_list(request):
+    return render(request, 'ku_market_place/order_list.html')
+
+
+def product(request):
+    return render(request, 'ku_market_place/products.html')
+
+
+def single_product(request):
+    return render(request, 'ku_market_place/single-product.html')
