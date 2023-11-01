28ebd72e288a5d3236a52179745294bb09173dc8
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-01 23:18:12 +0700

    Change Email field

--- ku_market_place/models/customer.py
+++ ku_market_place/models/customer.py
@@ -4,7 +4,7 @@ from django.db import models
 class Customer(models.Model):
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
     password = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
