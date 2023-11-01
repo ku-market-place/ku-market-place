afab8fe6599c9518d51563a3d32ca6d20ab7f3d2
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-01 23:14:23 +0700

    Add admin

--- ku_market_place/admin.py
+++ ku_market_place/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from ku_market_place.models import *
 
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Shipping)
+admin.site.register(Customer)
